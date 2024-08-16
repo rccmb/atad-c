5948a0854643e67e6f9906e62dea371ac1061e8e
Rodrigo <rodrigobaptistarodrigo@gmail.com>
2024-08-16 17:39:18 +0100

    Non-working.

--- adts/setArrayList.c
+++ adts/setArrayList.c
@@ -1,7 +1,43 @@
+/**
+ * @file setArrayList.c
+ * 
+ * @brief Provides an implementation of the ADT Set with an array list
+ * as the underlying data structure.
+ * 
+ * @author Rodrigo Baptista (202200217@estudantes.ips.pt)
+ * @bug No known bugs.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "set.h"
 
+#define INITIAL_CAPACITY 20
+
+typedef struct setImpl {
+  SetElem *elements;
+  int size;
+  int capacity;
+} SetImpl;
+
+static bool ensureCapacity(PtSet set);
+
 PtSet setCreate() {
-  // TODO
+  PtSet set = (PtSet) malloc(sizeof(SetImpl));
+  if(set == NULL) return NULL;
+
+  set->elements = (SetElem*) calloc(INITIAL_CAPACITY, sizeof(SetElem));
+
+  if(set->elements == NULL) {
+    free(set);
+    return NULL;
+  }
+
+  set->size = 0;
+  set->capacity = INITIAL_CAPACITY;
+
+  return set;
 }
 
 int setAdd(PtSet set, SetElem elem) {
@@ -42,4 +78,18 @@ void setPrint(PtSet set) {
 
 int setDestroy(PtSet *ptSet) {
   // TODO
+}
+
+static bool ensureCapacity(PtSet set) {
+  if(set->size == set->capacity) {
+    int newCap = set->capacity * 2;
+    SetElem *newElems = (SetElem*) realloc(set->elements, sizeof(SetElem) * newCap);
+
+    if(newElems == NULL) return false;
+
+    set->elements = newElems;
+    set->capacity = newCap;
+  }
+
+  return true;
 }
---
+++ adts/setElem.c
@@ -0,0 +1,7 @@
+/**
+ * @file setElem.c
+ * @brief Implements operations for SetElem.
+ * 
+ * @author Rodrigo Baptista (202200217@estudantes.ips.pt)
+ * @bug No known bugs.
+ */
